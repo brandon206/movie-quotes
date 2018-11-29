ca859f4043bc16bd04195c3d4766b85a50333442
Brandon Park <brandonpark206@gmail.com>
2018-11-28 18:09:25 -0800

    basic user auth HOC completed

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import auth from '../hoc/auth';
 
-export default props =>  (
-    <div>
-        <h1 className = "center">Movie Quotes</h1>
-        <h5>I'll be there for you</h5>
-    </div>
-);
+class Quotes extends Component {
+
+    render () {
+        
+        return (
+            <div>
+                <h1 className = "center">Movie Quotes</h1>
+                <h5>I'll be there for you</h5>
+            </div>
+        );
+    }
+}
+
+export default auth(Quotes);
